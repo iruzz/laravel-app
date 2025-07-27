9997c2037fc1aac59578edf8866fe98be0b5b00d
iruzz <rabbaniruzz45@gmail.com>
2025-07-27 17:13:37 +0700

    layout template

--- resources/views/siswa/detail.blade.php
+++ resources/views/siswa/detail.blade.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Ini adalah id ke {{ $nomer }}
-</body>
-</html>
+@extends('layout.master')
+
+@section('content')
+        <div class="container mt-5">
+        <h2 class="mb-4 text-center">Detail Siswa</h2>
+
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title   ">ID Siswa: {{ $nomer }}</h5>
+                <p class="card-text">Ini adalah detail siswa dengan ID: {{ $nomer }}</p>
+                <a href="/siswa" class="btn btn-primary">Kembali ke Daftar Siswa</a>
+            </div>
+        </div>
+@endsection
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Daftar Siswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <div class="container mt-5">
-        <h2 class="mb-4 text-center">Daftar Siswa</h2>
+@extends('layout.master')
+
+@section('content')
+           <h2 class="mb-4 text-center">Daftar Siswa</h2>
 
         <table class="table table-bordered table-striped">
             <thead class="table-dark">
@@ -36,6 +31,4 @@
                 @endforelse
             </tbody>
         </table>
-    </div>
-</body>
-</html>
+@endsection
