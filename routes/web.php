322710cf5638c861fcfe265421bad394143f1a21
iruzz <rabbaniruzz45@gmail.com>
2025-08-01 19:09:01 +0700

    tugas buat biodata di /profil

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\SiswaController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/profil', function () {
+    return view('profil');
 });
 
 Route::get('/siswa', 
