40c12924941290cbeb8f97ad6da22352c8bcc8a5
iruzz <rabbaniruzz45@gmail.com>
2025-08-02 19:36:19 +0700

    model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
-{
-    //
+{ 
+    
+     protected $fillable = [
+        'image',
+        'title',
+        'description',
+        'price',
+        'stock',
+    ];
 }
